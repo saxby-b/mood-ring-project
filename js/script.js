cb33f7440c0509ab0ce5b816a4dd22db48930bdf
C. Saxby Bain <csaxbybain@yahoo.com>
2024-11-19 12:47:17 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var moodList = document.querySelector("select");
+var mood = "happy";
+
+moodList.addEventListener("change", function (e) {
+    mood = e.target.value; 
+   if (mood === "happy") {
+      document.body.classList.remove("sad", "passionate");
+      document.body.classList.add("happy");
+   } else if (mood === "sad") {
+      document.body.classList.remove("happy", "passionate");
+      document.body.classList.add("sad");
+   } else if (mood === "passionate") {
+      document.body.classList.remove("sad", "happy");
+      document.body.classList.add("passionate");
+   }
+});
